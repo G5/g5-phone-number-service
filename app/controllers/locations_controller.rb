23f5f2c0f78744587591fbe454a3faba4ea170f3
Chad Crissman <cdcrissman@gmail.com>
2013-09-09 14:57:33 -0700

    add new location form to index

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,6 +3,7 @@ class LocationsController < ApplicationController
 
   def index
     @locations = Location.all
+    @location = Location.new
   end
 
   def show
@@ -20,7 +21,7 @@ class LocationsController < ApplicationController
 
     respond_to do |format|
       if @location.save
-        format.html { redirect_to @location, notice: 'Location was successfully created.' }
+        format.html { redirect_to locations_path, notice: 'Location was successfully created.' }
         format.json { render action: 'show', status: :created, location: @location }
       else
         format.html { render action: 'new' }
@@ -32,11 +33,11 @@ class LocationsController < ApplicationController
   def update
     respond_to do |format|
       if @location.update(location_params)
-        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+        format.html { redirect_to locations_path, notice: 'Location was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
+        format.json { render json: @locations.errors, status: :unprocessable_entity }
       end
     end
   end
