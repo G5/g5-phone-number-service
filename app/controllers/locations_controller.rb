b3e72314fe4ee520860dcbffa7873e34cfbcd251
Chad Crissman <cdcrissman@gmail.com>
2013-09-09 15:38:35 -0700

    removes untracked files from git

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -21,7 +21,7 @@ class LocationsController < ApplicationController
 
     respond_to do |format|
       if @location.save
-        format.html { redirect_to locations_path, notice: 'Location was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Location was successfully created.' }
         format.json { render action: 'show', status: :created, location: @location }
       else
         format.html { render action: 'new' }
@@ -33,7 +33,7 @@ class LocationsController < ApplicationController
   def update
     respond_to do |format|
       if @location.update(location_params)
-        format.html { redirect_to locations_path, notice: 'Location was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Location was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
