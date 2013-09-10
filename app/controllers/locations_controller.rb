a6f34f7c9e025d671e411544dc3573b1cd2a1b58
Chad Crissman <cdcrissman@gmail.com>
2013-09-10 16:07:56 -0700

    remove show edit before filters

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,5 @@
 class LocationsController < ApplicationController
-  before_action :set_location, only: [:show, :edit, :update, :destroy]
+  before_action :set_location, only: [:update, :destroy]
 
   def index
     @locations = Location.all
