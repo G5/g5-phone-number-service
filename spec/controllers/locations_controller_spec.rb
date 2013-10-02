52cef577fdf753d05e2cfd67872199d162c7b3d7
Chad Crissman <cdcrissman@gmail.com>
2013-10-02 12:28:30 -0700

    adds tests for creating location

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -5,13 +5,13 @@ describe LocationsController do
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
       get :index
-      response.should be_success
-      response.status.should eq(200)
+      expect(response).to be_success
+      expect(response.status).to eq(200)
     end
 
     it "renders the index template" do
       get :index
-      response.should render_template(:index)
+      expect(response).to render_template(:index)
     end
 
     it "loads all locations into @locations" do
@@ -20,9 +20,39 @@ describe LocationsController do
       expect(assigns(:locations)).to match_array([location1, location2])
     end
   end
-  
+
   describe "POST #create" do
-    
+    describe "when it saves" do
+      it "redirects to root path" do
+        Location.any_instance.stub(:save).and_return(true)
+        LocationsController.any_instance.stub(:location_params).and_return {}
+        post :create
+        expect(response).to redirect_to(root_path)
+        flash[:notice].should eq "Location was successfully created."
+      end
+    end
+
+    describe "when it does not save" do
+      it "redirects to root path" do
+        Location.any_instance.stub(:save).and_return(false)
+        LocationsController.any_instance.stub(:location_params).and_return {}
+        post :create
+        expect(response).to redirect_to(root_path)
+        flash[:notice].should eq "Location was NOT successfully created."
+      end
+    end
+
+    describe "when it's given invalid params" do
+      it "raises an error" do
+        expect {post :create}.to raise_error(ActionController::ParameterMissing)
+      end
+    end
+
+    describe "when it's given valid params" do
+      it "does not raise an error" do
+        expect {post :create, :location => {urn: "g5-cl-whatever-3"}}.not_to raise_error()
+      end
+    end
   end
 
 end
