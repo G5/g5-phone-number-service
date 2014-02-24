69176d455ccb25b7feb50c942d37a90c785f123e
jessicard <jessicard@mac.com>
2014-02-24 14:10:43 -0800

    adds basic auth env vars to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
+puts "spec_helper"
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+ENV["HTTP_BASIC_AUTH_NAME"] ||= "name"
+ENV["HTTP_BASIC_AUTH_PASSWORD"] ||= "password"
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require "capybara/rails"
