8cd54df2d1c853531ade6e7d7a9ca3eec98392bf
bbauer <brian.bauer@g5platform.com>
2014-06-30 17:03:36 -0700

    Remove old updater code

--- config/initializers/env.rb
+++
@@ -1,1 +0,0 @@
-ENV["G5_HUB_ENDPOINT"] ||= "http://g5-hub.herokuapp.com/clients/"
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ G5PhoneNumberService::Application.routes.draw do
   mount G5Updatable::Engine => '/g5_updatable'
   resources :locations
   root 'locations#index'
-
-  post "update" => "webhooks#update"
 end
