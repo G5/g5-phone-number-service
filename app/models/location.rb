2be28926f3df5f8540dc67e55a775735ff02d824
bbauer <brian.bauer@g5platform.com>
2014-11-16 15:29:55 -0800

    Allow numbers in location urn name

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
   validates :urn, :name, :default_number, presence: true
   validates :urn, uniqueness: true
-  validates_format_of :urn, with: /g5-cl-[A-Za-z0-9]{7,8}-[A-Za-z\-]+/
+  validates_format_of :urn, with: /g5-cl-[A-Za-z0-9]{7,8}-[A-Za-z0-9\-]+/
 end
