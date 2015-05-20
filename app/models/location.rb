a970f2a3192108448841c6e8f71ff8451f8ca0dc
johnlucia <johnmlucia@gmail.com>
2015-05-20 13:42:00 -0700

    staging validates client

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,8 @@ class Location < ActiveRecord::Base
   validates_format_of :urn, with: /g5-cl-[A-Za-z0-9]{7,8}-[A-Za-z0-9\-]+/
 
   belongs_to :client
+
+  # Need to turn this on eventually, but it wrecks a bunch of specs, so holding off for now.
+  # validates_presence_of :client
+
 end
