ad286b199586cc4055dbfe44d48636b238a56260
johnlucia <johnmlucia@gmail.com>
2015-05-21 09:21:43 -0700

    get the new/update/delete stuff working for clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -9,9 +9,49 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       format.html 
-
       # using { render json: @locations } adds an extra level of nesting that we don't want
       format.json { render json: @locations.to_json }
     end
   end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+
+    if @client.save
+      redirect_to @client, notice: 'Client was successfully created.'
+    else
+      render action: 'new'
+    end
+    
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    if @client.update(client_params)
+      redirect_to @client, notice: 'Client was successfully updated.'
+    else
+      render action: 'edit'
+    end 
+  end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def destroy
+    @client = Client.find(params[:id])
+    @client.destroy
+    redirect_to clients_url
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:urn, :name)
+  end
+
 end
