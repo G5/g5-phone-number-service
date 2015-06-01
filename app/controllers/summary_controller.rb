134200f9537ae94f5dfd532a84943a2489ed3011
johnlucia <johnmlucia@gmail.com>
2015-06-01 10:02:04 -0700

    dont need the summary controller any more

--- app/controllers/summary_controller.rb
+++
@@ -1,8 +0,0 @@
-class SummaryController < ApplicationController
-  http_basic_authenticate_with(name: ENV["HTTP_BASIC_AUTH_NAME"], password: ENV["HTTP_BASIC_AUTH_PASSWORD"], except: [:index, :show]) if ENV["HTTP_BASIC_AUTH_NAME"] && ENV["HTTP_BASIC_AUTH_PASSWORD"]
-
-  def index
-    @data = Location.all
-    render json: @data
-  end
-end
