d27c74d4fa3f9b8c1fc2402dd579288725a49c0f
johnlucia <johnmlucia@gmail.com>
2015-07-16 17:14:00 -0700

    some specs for that unique cpm code stuff

--- spec/models/ppc_number_spec.rb
+++ spec/models/ppc_number_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe PpcNumber do
-  
+  it "requires a cpm code" do
+    PpcNumber.new(cpm_code: "", number: "123-456-7890").should_not be_valid
+  end
+
+  it "requires a number" do
+    PpcNumber.new(cpm_code: "abc123", number: "").should_not be_valid
+  end
+
+  it "enforces unique cpm codes per location" do
+    PpcNumber.create(cpm_code: "abc123", number: "123-456-7890", location_id: 1)
+    PpcNumber.new(cpm_code: "abc123", number: "987-654-3210", location_id: 1).should_not be_valid
+  end
 end
