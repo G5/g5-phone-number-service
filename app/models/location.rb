beb19dd8c9f72cb5aa87c5e5d3de1c6d45f7743b
Dave Reuss <dave.reuss@getg5.com>
2016-04-19 12:08:00 -0700

    Dial - country code

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,8 +2,22 @@ class Location < G5Updatable::Location
   has_many :phone_numbers
   has_many :ppc_numbers
 
+  # Add new Countries Here, that are not US or CA, or we could add country codes in the Hub?
+  COUNTRY_DATA = {  "AU" => 
+                      { 
+                        country_code: "61"
+                      }
+                  }.with_indifferent_access
+
   def country_code_prefix
-    country_code = properties['country_code']
+    country = properties['country']
+    match_country = COUNTRY_DATA.fetch(country, "")
+    country_code = match_country.blank? ? "1" : match_country[:country_code]
     "+#{country_code}-"
   end
+
+  # def country_code_prefix
+  #   country_code = properties['country_code']
+  #   "+#{country_code}-"
+  # end
 end
